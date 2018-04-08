f71b93804436817d51cf2e213ab528c6478b54f2
NoahKarran <38185401+NoahKarran@users.noreply.github.com>
2018-04-08 19:52:45 +0100

    Update Server.java

--- Code/SERVER/Server.java
+++ Code/SERVER/Server.java
@@ -39,7 +39,7 @@ public class Server extends JFrame{
     //Set up and run the server
     public void startRunning(){
         try{
-           server = new ServerSocket(6789, 100);
+           server = new ServerSocket(1111, 100);
            while(true){
                try{
                    waitForConnection();
